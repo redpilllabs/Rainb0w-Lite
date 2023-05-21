aef811cd7378442c49e97cb1c0b8ba92651e0403
Neu <neusradioxxx@gmail.com>
2023-05-21 13:12:28 +0330

    Revert commit 73ece59

--- run.sh
+++ run.sh
@@ -23,10 +23,6 @@ else
         if [ ! "$DISTRO_VERSION" == "20.04" ] && [ ! "$DISTRO_VERSION" == "22.04" ]; then
             echo "Your version of Ubuntu is not supported! Only 20.04 and 22.04 versions are supported."
             exit 0
-        else
-            # Purging Snap to free up some memory...
-            fn_check_and_remove_pkg snapd
-            systemctl daemon-reload
         fi
     elif [[ "$DISTRO" =~ "Debian GNU/Linux" ]]; then
         if [ ! "$DISTRO_VERSION" == "11" ]; then
